de151e5d8925b94740d090d0330d5eefb4aa90bc
Sven <svo@otobo.org>
2023-02-16 15:13:52 +0100

    Rename Filter module, small default config changes.

--- Kernel/System/PostMaster/Filter/SkipViaMessageID.pm
+++ Kernel/System/PostMaster/Filter/SkipViaMessageID.pm
@@ -14,7 +14,7 @@
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 # --
 
-package Kernel::System::PostMaster::Filter::MatchMessageID;
+package Kernel::System::PostMaster::Filter::SkipViaMessageID;
 
 use strict;
 use warnings;
